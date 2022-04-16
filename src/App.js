855473e3dfa58db73975af387888ee45f0c2307b
andreyostroglyad <andrey@triber.app>
2022-04-16 12:43:34 +0300

    - form started

--- src/App.js
+++ src/App.js
@@ -6,21 +6,27 @@ import BookingsPage from "./pages/Bookings";
 import EventsPage from "./pages/Events";
 import LoginPage from "./pages/Login";
 import NotFoundPage from "./pages/NotFound";
+import MainNavigation from "./components/navigation/MainNavigation";
 
 
 class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Navigate from="/" to="login" replace/>}/>
-                    <Route exact path="/login" element={<LoginPage/>}/>
-                    <Route path="/users" element={<UsersPage/>}/>
-                    <Route path="/events" element={<EventsPage/>}/>
-                    <Route path="/bookings" element={<BookingsPage/>}/>
-                    <Route path="/recovery-password" element={null}/>
-                    <Route path="*" element={<NotFoundPage/>}/>
-                </Routes>
+                <React.Fragment>
+                    <MainNavigation />
+                    <main className="main-content">
+                        <Routes>
+                            <Route path="/" element={<Navigate from="/" to="login" replace/>}/>
+                            <Route exact path="/login" element={<LoginPage/>}/>
+                            <Route path="/users" element={<UsersPage/>}/>
+                            <Route path="/events" element={<EventsPage/>}/>
+                            <Route path="/bookings" element={<BookingsPage/>}/>
+                            <Route path="/recovery-password" element={null}/>
+                            <Route path="*" element={<NotFoundPage/>}/>
+                        </Routes>
+                    </main>
+                </React.Fragment>
             </BrowserRouter>
         );
     }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
+import "./Login.css";
 
 class LoginPage extends Component {
     render() {
-        return <h1>The first React LoginPage</h1>
+        return <form className="auth-form">
+            <div className="form-control">
+                <label htmlFor="email">E-mail</label>
+                <input type="email" id="email"/>
+            </div>
+            <div className="form-control">
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password"/>
+            </div>
+            <div className="form-actions">
+                <button type="button">Switch to Signup</button>
+                <button type="button">Forgot password</button>
+                <button type="submit">Submit</button>
+            </div>
+        </form>
     }
 }
 
